f1f229b7a29c3564e6e8da3916e73abff5b24b77
Jarstii <xy_jxnu@163.com>
2026-01-06 19:38:37 +0800

    add device log

--- common/utils.py
+++ common/utils.py
@@ -10,5 +10,8 @@ def get_device(cuda_num: int = 0) -> Device:
 
     if torch.cuda.is_available():
         device = f"cuda:{cuda_num}"
+    print(40 * "=")
+    print(f"Using device: {device}")
+    print(40 * "=")
 
     return device
